0eaff1c5db4342cd65c900ad086cd46214da4f0f
Vlad <vlad@ghn.me>
2020-02-21 09:24:55 -0600

    Add Slack Alerts Webhook

--- variables.tf
+++ variables.tf
@@ -1,17 +1,26 @@
 variable "email" {
   description = "Notifications email"
+  type        = string
+}
+
+variable "slack_alerts_hook_url" {
+  description = "Slack Alerts Channel Hook URL"
+  type        = string
 }
 
 variable "cloudflare_email" {
   description = "CloudFlare EMail"
+  type        = string
 }
 
 variable "cloudflare_api_key" {
   description = "CloudFlare API Key"
+  type        = string
 }
 
 variable "cloudflare_zone_id" {
   description = "CloudFlare Zone ID"
+  type        = string
 }
 
 variable "common_tags" {
