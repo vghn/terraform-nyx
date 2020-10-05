1576e7ffe05119eb73fa2e04adb01c12deb6b879
Vlad <vlad@ghn.me>
2020-10-05 10:16:09 -0500

    Pin provider versions

--- versions.tf
+++ versions.tf
@@ -1,19 +1,24 @@
 terraform {
   required_providers {
     archive = {
-      source = "hashicorp/archive"
+      source  = "hashicorp/archive"
+      version = "~> 1.0"
     }
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
     cloudflare = {
-      source = "cloudflare/cloudflare"
+      source  = "cloudflare/cloudflare"
+      version = "~> 2.0"
     }
     null = {
-      source = "hashicorp/null"
+      source  = "hashicorp/null"
+      version = "~> 2.0"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
+      version = "~> 2.0"
     }
   }
   required_version = ">= 0.13"
